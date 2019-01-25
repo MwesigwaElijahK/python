3962d3dfacae4248a54f209cebf177f35a85fb70
MwesigwaElijahK <46490182+MwesigwaElijahK@users.noreply.github.com>
2019-01-24 23:39:59 -0900

    practice

---
+++ vowel.py
@@ -0,0 +1,43 @@
+# def sayhi(country, title):
+#     print("jambo " + country + ",you are the " + title)
+
+# sayhi("Uganda", "motherland")
+# sayhi("Kenya", "neighbour")
+
+# def square(no):
+#     return no*no
+
+# print(square(6))    
+
+
+
+# y1 = float(input("enter first number: ")) 
+# op = input("enter operand: ") 
+# y2 = float(input("enter second number: ")) 
+
+# if op == "+" :
+#     print(y1 + y2)
+# elif op == "-" :
+#     print(y1 - y2) 
+# elif op == "/" :
+#     print(y1 / y2)
+# elif op == "*" :
+#     print(y1 * y2)       
+# else:
+#     print("invalid operand")
+
+
+
+
+# e = 50                                             
+# while e >= 25:
+#     print(e) 
+#     e -= 1
+# print("the_el!") 
+   
+
+e = 20
+while e <= 50:
+    print(e) 
+    e += 1
+print("the_el!")
